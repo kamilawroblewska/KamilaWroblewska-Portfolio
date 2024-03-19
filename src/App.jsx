386546fe822cb0542acdfc101e3cbd27518730ac
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-19 17:00:42 +0000

    Added router to app.jsx to connect all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import Home from './Components/home';
+import Resume from './Components/resume';
+import Projects from './components/projects';
+import Contact from './components/contactontact';
 
 function App() {
  
   return (
-     <></>
+     <>
+      <Router>
+        <div>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/resume" element={<Resume />} />
+            <Route exact path="/projects" element={<Projects />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer/>
+        </div>
+      </Router>
+      </>
   )
 }
 
