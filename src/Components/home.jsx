9cdaf837981477177352a2aa65aa20caef76a1c6
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-19 18:39:43 +0000

    Added react type animation; added welcome messege on home page

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 // Functional component to display header
 const Home = () => {
     // Returning the header with navigation bar
     return (
-      <></>
+      <>
+      <TypeAnimation
+        sequence={[
+          `Welcome to my Portfolio!\nI'm a inspiring Front -End Developer\nand a Designer.`,
+          1000,
+          "",
+        ]}
+        speed={50}
+        style={{ whiteSpace: 'pre-line', fontSize: '2em' }}
+        repeat={Infinity}
+      />
+      </>
     );
   };
   
