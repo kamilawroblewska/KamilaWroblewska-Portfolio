b4bb44bace9cae9c7513a9bf6a3aeb7af4e9ff0b
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-20 18:34:58 +0000

    Added portfolio and Projects components; added screenshots and working links for deployed page and github link

---
+++ src/Components/project.jsx
@@ -0,0 +1,27 @@
+import React from 'react'; 
+import '../styles/project.css';
+
+
+const Project = ({ title, image, deployedLink, githubLink }) => {
+  
+  return (
+    <article className='col-md-4 p-2'>
+      <section className='card project-card'>
+        <div className='project-title'>
+          <h3 className='m-2'>{title}</h3>
+        </div>
+        <img src={image} alt={title} />
+        <div className='project-links py-1'>
+          <a href={deployedLink} target='_blank' rel='noopener noreferrer' className='btn btn-primary m-1'>
+            Deployed Application
+          </a>
+          <a href={githubLink} target='_blank' rel='noopener noreferrer' className='btn btn-secondary m-1'>
+            GitHub Repository
+          </a>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Project; 
