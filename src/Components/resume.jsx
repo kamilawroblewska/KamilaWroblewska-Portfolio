a64ee4df72ff8732423edc3c196ddec7a3f9fd25
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-03-20 20:18:24 +0000

    Added resume page, uploaded resume in pdf, styled page

--- src/Components/resume.jsx
+++ src/Components/resume.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import ResumeDownload from './Assets/KAMILAWROBLEWSKA.pdf'
+import '../styles/resume.css'
+import Screen from '../Components/Assets/ScreenshotResume.png'
 
-// Functional component to display header
 const Resume = () => {
-    // Returning the header with navigation bar
+
     return (
-      <></>
-    );
-  };
+      <section className='container mb-4 py-4 col-md-6 contact'>
+      <h1 className='text-center'>Resume</h1>
+      <div className='card p-3 mt-3'>
+      <img className='resume'
+      src={Screen}/>
+        <a href={ResumeDownload} download className='d-flex justify-content-center text-decoration-none'>
+          <button className='btn btn-primary formButton'>Download Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+};
   
   export default Resume;
